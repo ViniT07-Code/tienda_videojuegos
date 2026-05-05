2f64771e85e25bfb16b39e21c6f2a31554719d15
Marco Vinicio <maroviniciot07@gmail.com>
2026-05-04 18:08:17 -0600

    se agregaron archivos estaticos

--- proyecto/tienda_videojuegos/settings.py
+++ proyecto/tienda_videojuegos/settings.py
@@ -118,3 +118,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATTICFILES_DIRS = [BASE_DIR / 'static']
+
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField.BIGAUTOFIELD'
